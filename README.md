1f8a924e191da7f80e5f252779828bd8485cc70e
brunades <bruna.contino@outlook.com>
2024-07-16 15:07:03 +0200

    Exercises 3.1 - 3.8 completed

---
+++ README.md
@@ -0,0 +1,12 @@
+# University of Helsinki's Fullstack Open 2024 
+Standalone repository for part 3.
+
+## Part 3 - Programming a server with NodeJS and Express
+
+> In this part our focus shifts towards the backend, that is, towards implementing functionality on the server side of the stack. We will implement a simple REST API in Node.js by using the Express library, and the application's data will be stored in a MongoDB database. At the end of this part, we will deploy our application to the internet.
+
+### Exercises focus:
+
+Build a REST backend for the frontend UI 'Phonebook' built in the previous parts.
+
+[View Live App]
---
+++ index.js
@@ -0,0 +1,104 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+
+let persons = [
+  { 
+    id: 1,
+    name: "Arto Hellas", 
+    number: "040-123456"
+  },
+  { 
+    id: 2,
+    name: "Ada Lovelace", 
+    number: "39-44-5323523"
+  },
+  { 
+    id: 3,
+    name: "Dan Abramov", 
+    number: "12-43-234345"
+  },
+  { 
+    id: 4,
+    name: "Mary Poppendieck", 
+    number: "39-23-6423122"
+  }
+]
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => Number(n.id)))
+    : 0
+  return String(Math.floor((Math.random() * (1000 - (maxId)) + maxId +1)))
+}
+
+app.use(express.json())
+
+app.use(cors())
+
+morgan.token('personData', (req) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  }
+})
+
+
+app.use(morgan(':method :url :response-time :personData'))
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  const duplicate = persons.find(person => person.name.toLowerCase() === req.body.name.toLowerCase())
+
+  if (!req.body.name || !req.body.number) {
+    return res.status(400).json({
+      error: 'Name and number are required'
+    })
+  }
+
+  if(duplicate) {
+    return res.status(400).json({
+      error: 'Name already exists'
+    })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons.concat(person)
+
+  res.json(person)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+app.get('/api/info', (req, res) => {
+  const date = new Date
+  res.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${date}</p>`)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
